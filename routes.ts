a6e652da5ad5db430576917f9c03da121eb32292
Lorenzo Maiorfi <maiorfi@gmail.com>
2018-11-02 16:53:42 +0100

    Aggiunta di più verbi

--- routes.ts
+++ routes.ts
@@ -7,5 +7,46 @@ export class Routes {
         message: "GET request successful!"
       });
     });
+
+    // contact
+    app
+      .route("/contact")
+      // GET action
+      .get((req: Request, res: Response) => {
+        // get all contacts
+        res.status(200).send({
+          message: "GET request successful!"
+        });
+      })
+      // POST action
+      .post((req: Request, res: Response) => {
+        // create new contact
+        res.status(200).send({
+          message: "POST request successful!"
+        });
+      });
+
+    // contact detail
+    app
+      .route("/contact/:contactId")
+      // get specific contact
+      .get((req: Request, res: Response) => {
+        // get a single contact detail
+        res.status(200).send({
+          message: `GET request successful! (id=${req.params.contactId})`
+        });
+      })
+      .put((req: Request, res: Response) => {
+        // update a contact
+        res.status(200).send({
+          message: "PUT request successful!"
+        });
+      })
+      .delete((req: Request, res: Response) => {
+        // delete a contact
+        res.status(200).send({
+          message: `DELETE request successful! (id=${req.params.contactId})`
+        });
+      });
   }
 }
