9816212506eec87f119876f6b8d6857eaa0d5d00
Lorenzo Maiorfi <maiorfi@gmail.com>
2018-11-03 07:40:19 +0100

    Fix Port

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-const PORT:number = parseInt(process.env.HTTP_LISTENER_PORT);
+const PORT:number = parseInt(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
   console.log("Express server listening on port " + PORT);
