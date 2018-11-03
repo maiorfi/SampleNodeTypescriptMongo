4bbc18c4991865068fc50df9eab50576b283cdd7
Lorenzo Maiorfi <maiorfi@gmail.com>
2018-11-03 07:53:29 +0100

    Log PORT

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = parseInt(process.env.HTTP_LISTENER_PORT);
+const PORT = parseInt(process.env.PORT) || 5000;
+console.log(`PORT=${PORT}`);
 app_1.default.listen(PORT, () => {
     console.log("Express server listening on port " + PORT);
 });
--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import app from "./app";
 const PORT:number = parseInt(process.env.PORT) || 5000;
 
+console.log(`PORT=${PORT}`);
+
 app.listen(PORT, () => {
   console.log("Express server listening on port " + PORT);
 });
